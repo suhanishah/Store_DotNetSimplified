5a8e996857afde15acc7496734d0dfec7f517ee6
Suhani Shah <suhanishah@live.com>
2021-01-16 15:44:42 -0500

    CRUD Operations with Client and Server Side Validations

---
+++ Store/Controllers/CustomersController.cs
@@ -0,0 +1,87 @@
+﻿using Store.Models;
+using Store.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Store.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private StoreEntities db;
+        public CustomersController()
+        {
+            db = new StoreEntities();
+        }
+        // GET: Customers
+
+        public ActionResult CustomerList()
+        {
+            var viewModel = new CustomerViewModel
+            {
+                Customers = db.customers.ToList()
+            };
+            return View(viewModel);
+        }
+
+        public ActionResult CustomerForm()
+        {
+            var viewModel = new CustomerViewModel
+            {
+                Customer = new customer()
+            };
+            return View(viewModel);
+        }
+
+        public ActionResult EditCustomer(int id)
+        {
+            var viewModel = new CustomerViewModel
+            {
+                Customer = db.customers.Where(x => x.customer_id == id).SingleOrDefault()
+            };
+            return View("CustomerForm", viewModel);
+        }
+        public ActionResult Save(customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerViewModel
+                {
+                    Customer = customer
+                };
+                return View("CustomerForm", viewModel);
+            }
+
+            if (customer.customer_id == 0)
+            {
+                db.customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = db.customers.Where(x => x.customer_id == customer.customer_id).SingleOrDefault();
+                customerInDb.first_name = customer.first_name;
+                customerInDb.last_name = customer.last_name;
+                customerInDb.birth_date = customer.birth_date;
+                customerInDb.phone = customer.phone;
+                customerInDb.address = customer.address;
+                customerInDb.city = customer.city;
+                customerInDb.state = customer.state;                
+            }
+
+            db.SaveChanges();
+            return RedirectToAction("CustomerList");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteCustomer(int id)
+        {
+            var customerToDelete = db.customers.Where(x => x.customer_id == id).SingleOrDefault();
+            db.customers.Remove(customerToDelete);
+            db.SaveChanges();
+            return RedirectToAction("CustomerList");
+        }
+    }
+}
---
+++ Store/Models/Metadata/CustomerMetadata.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Store.Models
+{
+    [MetadataType(typeof(customer.Metadata))]
+    public partial class customer
+    {
+        sealed class Metadata
+        {
+            [Required]
+            [Display(Name ="First Name")]
+            public string first_name { get; set; }
+
+            [Required]
+            [Display(Name = "Last Name")]
+            public string last_name { get; set; }
+
+            [Display(Name ="Birth Date")]
+            public System.DateTime birth_date { get; set; }
+
+            [Phone]
+            [Required]
+            [Display(Name ="Phone")]
+            public string phone { get; set; }
+
+            [Required]
+            [Display(Name ="Address")]
+            public string address { get; set; }
+
+            [Required]
+            [Display(Name = "City")]
+            public string city { get; set; }
+
+            [Required]
+            [Display(Name = "State")]
+            public string state { get; set; }
+        }
+    }
+}
